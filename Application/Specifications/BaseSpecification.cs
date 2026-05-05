509b232afbd4bfd7dce469215f3574d9740b7161
Osss <Osamaabuzanh@gami.com>
2026-05-05 11:19:29 +0300

    feat:specification pattern

---
+++ Application/Specifications/BaseSpecification.cs
@@ -0,0 +1,40 @@
+﻿using Application.Repositories.Interface;
+using System.Linq.Expressions;
+
+namespace Application.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, object>>? OrderBy { get; private set; }
+        public Expression<Func<T,Object>>? OrderByDescending { get; private set; }
+        public int Take { get; private set; }
+        public int Skip { get; private set; }
+        public bool IsPagingEnabled { get; private set; }
+
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+        public void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+        public void ApplyOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+        public void ApplyOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+        {
+            OrderByDescending = orderByDescendingExpression;
+        }
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+    }
+}
---
+++ Application/Specifications/TodoSpecs/TodoWithFiltersSpecs.cs
@@ -0,0 +1,31 @@
+﻿using Application.Dtos.Todo;
+using Domain.Entities;
+
+namespace Application.Specifications.TodoSpecs
+{
+    public class TodoWithFiltersSpecs : BaseSpecification<Todo>
+    {
+        public TodoWithFiltersSpecs(TodoFilterDto filter, int userId, bool isAdmin)
+
+            : base(x => 
+               (isAdmin || x.UserId == userId) &&
+
+                (string.IsNullOrEmpty(filter.Search) || x.Title.Contains(filter.Search)) &&
+
+                (!filter.CategoryId.HasValue || x.CategoryId == filter.CategoryId) &&
+                (!filter.Priority.HasValue || x.Priority == filter.Priority) &&
+                (!filter.Status.HasValue || x.Status == filter.Status) &&
+                (!filter.RecurrenceType.HasValue || x.RecurrenceType == filter.RecurrenceType))
+                
+        {
+            AddInclude(x => x.Category);
+            AddInclude(x => x.User);
+            AddInclude(x => x.Attachments);
+
+            ApplyOrderByDescending(x => x.CreatedAt);
+
+            ApplyPaging((filter.PageNumber - 1) * filter.PageSize, filter.PageSize);
+        }
+
+    }
+}
